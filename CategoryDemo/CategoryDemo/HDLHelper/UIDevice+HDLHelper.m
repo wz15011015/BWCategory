f1c9ea0f7b0a2048bc6ddea508e704575cf636c4
wangzhi <wzqsyk@126.com>
2022-07-06 14:32:05 +0800

    更新设备型号。

--- CategoryDemo/CategoryDemo/HDLHelper/UIDevice+HDLHelper.m
+++ CategoryDemo/CategoryDemo/HDLHelper/UIDevice+HDLHelper.m
@@ -117,6 +117,11 @@
         @"iPhone13,2" : @"iPhone 12",
         @"iPhone13,3" : @"iPhone 12 Pro",
         @"iPhone13,4" : @"iPhone 12 Pro Max",
+        @"iPhone14,2" : @"iPhone 13 Pro",
+        @"iPhone14,3" : @"iPhone 13 Pro Max",
+        @"iPhone14,4" : @"iPhone 13 mini",
+        @"iPhone14,5" : @"iPhone 13",
+        @"iPhone14,6" : @"iPhone SE 3",
         
         // 2. iPod 系列
         @"iPod1,1" : @"iPod touch 1",
@@ -148,6 +153,8 @@
         @"iPad7,12" : @"iPad 7",
         @"iPad11,6" : @"iPad 8",
         @"iPad11,7" : @"iPad 8",
+        @"iPad12,1" : @"iPad 9",
+        @"iPad12,2" : @"iPad 9",
         // iPad Air
         @"iPad4,1" : @"iPad Air",
         @"iPad4,2" : @"iPad Air",
@@ -158,6 +165,8 @@
         @"iPad11,4" : @"iPad Air 3",
         @"iPad13,1" : @"iPad Air 4",
         @"iPad13,2" : @"iPad Air 4",
+        @"iPad13,16" : @"iPad Air 5",
+        @"iPad13,17" : @"iPad Air 5",
         // iPad Pro
         @"iPad6,3" : @"iPad Pro (9.7-inch)",
         @"iPad6,4" : @"iPad Pro (9.7-inch)",
@@ -175,8 +184,18 @@
         @"iPad8,6" : @"iPad Pro 3 (12.9-inch)",
         @"iPad8,7" : @"iPad Pro 3 (12.9-inch)",
         @"iPad8,8" : @"iPad Pro 3 (12.9-inch)",
+        @"iPad8,9"  : @"iPad Pro 2 (11-inch)",
+        @"iPad8,10" : @"iPad Pro 2 (11-inch)",
         @"iPad8,11" : @"iPad Pro 4 (12.9-inch)",
         @"iPad8,12" : @"iPad Pro 4 (12.9-inch)",
+        @"iPad13,4" : @"iPad Pro 3 (11-inch)",
+        @"iPad13,5" : @"iPad Pro 3 (11-inch)",
+        @"iPad13,6" : @"iPad Pro 3 (11-inch)",
+        @"iPad13,7" : @"iPad Pro 3 (11-inch)",
+        @"iPad13,8"  : @"iPad Pro 5 (12.9-inch)",
+        @"iPad13,9"  : @"iPad Pro 5 (12.9-inch)",
+        @"iPad13,10" : @"iPad Pro 5 (12.9-inch)",
+        @"iPad13,11" : @"iPad Pro 5 (12.9-inch)",
         // iPad mini
         @"iPad2,5" : @"iPad mini 1 (WiFi)",
         @"iPad2,6" : @"iPad mini 1 (GSM)",
@@ -191,6 +210,8 @@
         @"iPad5,2" : @"iPad mini 4",
         @"iPad11,1" : @"iPad mini 5",
         @"iPad11,2" : @"iPad mini 5",
+        @"iPad14,1" : @"iPad mini 6",
+        @"iPad14,2" : @"iPad mini 6",
         
         // 4. Apple Watch 系列
         @"Watch1,1" : @"Apple Watch 38mm",
@@ -219,18 +240,31 @@
         @"Watch6,2" : @"Apple Watch Series 6 44mm",
         @"Watch6,3" : @"Apple Watch Series 6 40mm",
         @"Watch6,4" : @"Apple Watch Series 6 44mm",
+        @"Watch6,6" : @"Apple Watch Series 7 41mm",
+        @"Watch6,7" : @"Apple Watch Series 7 45mm",
+        @"Watch6,8" : @"Apple Watch Series 7 41mm",
+        @"Watch6,9" : @"Apple Watch Series 7 45mm",
         
         // 5. Apple TV 系列
+        @"AppleTV1,1" : @"Apple TV 1",
         @"AppleTV2,1" : @"Apple TV 2",
         @"AppleTV3,1" : @"Apple TV 3",
         @"AppleTV3,2" : @"Apple TV 3",
         @"AppleTV5,3" : @"Apple TV 4",
-        @"AppleTV6,2" : @"Apple TV 4K",
+        @"AppleTV6,2"  : @"Apple TV 4K",
+        @"AppleTV11,1" : @"Apple TV 4K 2",
         
         // 6. AirPods 系列
         @"AirPods1,1" : @"AirPods 1",
+        @"AirPods1,2" : @"AirPods 2",
         @"AirPods2,1" : @"AirPods 2",
-        @"iProd8,1"   : @"AirPods Pro",
+        @"AirPods1,3" : @"AirPods 3",
+        @"Audio2,1"   : @"AirPods 3",
+        @"AirPods2,2"    : @"AirPods Pro",
+        @"AirPodsPro1,1" : @"AirPods Pro",
+        @"iProd8,1"      : @"AirPods Pro",
+        @"iProd8,6"      : @"AirPods Max",
+        @"AirPodsMax1,1" : @"AirPods Max",
         
         // 7. HomePod 系列
         @"AudioAccessory1,1" : @"HomePod",
@@ -247,6 +281,10 @@
         // 10. Siri Remote 系列 (暂时未知)
 //        @"Unknown" : @"Siri Remote",
 //        @"Unknown" : @"Siri Remote 2",
+//        @"Unknown" : @"Siri Remote 3",
+        
+        // 11. AirTag
+        @"AirTag1,1" : @"AirTag",
 
         // 模拟器
         @"i386"   : @"Simulator x86",
